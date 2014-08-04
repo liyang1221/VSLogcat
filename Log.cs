69c664dd62726142c06d02e91f4e8b9e3fe1328d
RugbbyLi <19198926@qq.com>
2014-08-04 22:11:44 +0800

    -update readme

---
+++ Log.cs
@@ -0,0 +1,30 @@
+﻿namespace ly
+{
+    static class Log
+    {
+        public static void V(string message)
+        {
+            System.Diagnostics.Debug.WriteLine(string.Format("[verbose]{0}", message));
+        }
+
+        public static void E(string message)
+        {
+            System.Diagnostics.Debug.WriteLine(string.Format("[error]{0}", message));
+        }
+
+        public static void W(string message)
+        {
+            System.Diagnostics.Debug.WriteLine(string.Format("[warn]{0}", message));
+        }
+
+        public static void I(string message)
+        {
+            System.Diagnostics.Debug.WriteLine(string.Format("[info]{0}", message));
+        }
+
+        public static void D(string message)
+        {
+            System.Diagnostics.Debug.WriteLine(string.Format("[debug]{0}", message));
+        }
+    }
+}
